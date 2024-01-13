217d8763f016b9f000fb81a1b6de1e79e87e38bb
hassan34319 <mh08062@st.habib.edu.pk>
2024-01-13 23:18:24 +0500

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
+import { styles } from "./styles";
 
 import {
   About,
@@ -21,6 +22,26 @@ const App = () => {
           <div className="hidden lg:block">
             <Hero />
           </div>
+          <section className={`relative w-full h-max pb-80 mx-auto md:hidden`}>
+      <div
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className='text-[#915EFF]'>Hassan</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop User friendly <br className='sm:block hidden' />
+             Web applications
+          </p>
+        </div>
+      </div>
+      </section>
         </div>
         <About />
         <Experience />

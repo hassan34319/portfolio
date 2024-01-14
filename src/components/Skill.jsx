1574b575472271ffb4df165b600db124f11eb524
hassan34319 <mh08062@st.habib.edu.pk>
2024-01-14 14:02:16 +0500

    Added padding

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -16,7 +16,7 @@ function Skill({ directionLeft,src,percentage }) {
           x: 0,
         }}
         transition={{ duration: 1 }}
-        className="rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter md:w-28 md:h-28 group-hover:grayscale transition duration-300 ease-in-out"
+        className="rounded-full border border-gray-500 p-2 object-cover w-24 h-24 xl:w-32 xl:h-32 filter md:w-28 md:h-28 group-hover:grayscale transition duration-300 ease-in-out"
         src={src}
         alt=""
       />
